3456aef305134937ad05d0f4b28e2dcee2de26ad
jugraj <gurjeetaur0@gmail.com>
2020-03-11 17:13:42 -0700

    created a new form to add new grams to the grams table with new migration to add price column

---
+++ db/migrations/20200311161848_addingPriceColumn.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.table('grams', function(t) {
+        t.integer('price');
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table('grams', function(t) {
+        t.dropColumn('price');
+    });
+};
--- routes/grams.js
+++ routes/grams.js
@@ -2,11 +2,44 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../db/client')
 
-router.get('/', (req, res) => {
-    knex.select('*').from('grams')
-      .then(grams => {
-        res.render('users/index', { grams })
-      })
+  router.get('/grams', (req, res) => {
+      knex.select('*').from('grams')
+        .then(grams => {
+          res.render('users/index', { grams })
+        })
+    })
+
+  router.get('/users/new', (req, res)=>{
+    res.render('users/new.ejs')
+  })
+ 
+  router.get('/grams/new', (req, res)=>{
+    res.render('grams/new.ejs')
   })
   
+  router.post('/grams', (req, res) => {
+    const 
+  { srcUrl,
+    description,
+    price
+  } = req.body
+  if(!srcUrl){
+    res.send('please provide an URL')
+  }else{
+    knex.insert({ 
+      srcUrl,
+      description,
+      price
+    }).into('grams')
+      .then(() => {
+        res.redirect('/grams')
+      })
+      .catch(() => {
+        res.render('users/new')
+      })
+  }
+   
+  })
+  
+
 module.exports = router
